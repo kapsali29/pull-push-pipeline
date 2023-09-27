6811c5481dc31cc0aa05d4178c163f09d9bcfa33
kapsali29 <kapsali29@gmail.com>
2023-09-27 17:03:44 +0200

    k8s publisher added to project

--- k8s_publisher/test.py
+++
@@ -1,7 +0,0 @@
-from prometheus_client.k8s_metrics import K8SMetrics
-
-metrics_str_tuple = ('node_', 'container_', 'kubelet_')
-prometheus_base_url = 'localhost:8000'
-k8s = K8SMetrics(prometheus_base_url, metrics_str_tuple)
-results = k8s.fetch_relevant_metrics()
-print(results)
