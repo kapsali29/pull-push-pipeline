94ba7f296ccd18be560fa3db2cd76560c5f45e6a
kapsali29 <kapsali29@gmail.com>
2023-10-12 13:31:06 +0200

    various changes in structure and code

--- scripts/flows/fetch_metrics_tiny_set.py
+++ scripts/flows/fetch_metrics_tiny_set.py
@@ -2,6 +2,8 @@ from time import sleep
 
 from prefect import flow, get_run_logger, task
 from prometheus.k8s_metrics import K8SMetrics
+from kafka.client import KafkaObject
+
 
 @task
 def init_client():
@@ -11,23 +13,40 @@ def init_client():
     )
     return k8s_client
 
+
+@task
+def init_kafka_object():
+    kafka_obj = KafkaObject(
+        bootstrap_servers="broker:29092"
+    )
+    return kafka_obj
+
+
 @task
 def fetch_k8s_metrics(k8s_client):
     metrics_list = k8s_client.fetch_relevant_metrics()
     return metrics_list
 
+
 @task
 def get_metric_value(k8s_client, metric):
     response = k8s_client.intent_query(metric)
     response_data = response.json()
-    logger = get_run_logger()
-    logger.info(response_data)
+    return response_data
+
 
 @flow
 def fetch_metrics_subset():
     k8s_client = init_client()
+    kafka_obj = init_kafka_object()
     metrics_list = fetch_k8s_metrics(k8s_client)
-    
+
     for metric in metrics_list:
-        get_metric_value(k8s_client, metric)
-        
+        metric_data = get_metric_value(
+            k8s_client=k8s_client,
+            metric=metric
+        )
+        kafka_obj.producer(
+            msg=metric_data,
+            topic="tiny-example"
+        )
